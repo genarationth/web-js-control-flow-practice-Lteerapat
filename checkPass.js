608c6fbbd9398f0a10ceea9bb0ca0a95da31bb8c
Teerapat Limpawittayakul <tlimpawittayakul@gmail.com>
2023-03-13 14:42:13 +0700

    create checkPass.js

---
+++ checkPass.js
@@ -0,0 +1,2 @@
+const password = 'passsss';
+(password.length >= 12) ? console.log('Very Strong and Welcome!') : (password.length >= 8 ? console.log('Welcome! Home!') : console.log('Sorry Mate Too Weak!'));
